f7bf97be95c1cbabb0b89f4390b0587808f2d8ae
Adam Knight <adam@rakenapp.com>
2022-03-30 11:38:43 +1300

    Small improvements to the OTR prototype

--- client/views/Prototype.vue
+++ client/views/Prototype.vue
@@ -19,7 +19,8 @@
             ],
             payType: "",
             dailyLimit: null,
-            weeklyLimit: null
+            weeklyLimit: null,
+            json: null
           }
       },
       methods: {
@@ -32,16 +33,43 @@
           }
 
           this.rows.push({
+            uuid: "<UUID>",
+            order: this.rows.length,
             payType: this.payType
           })
 
           this.payType = ""
           this.dailyLimit = null
           this.weeklyLimit = null
+
+          this.updateRows();
         },
 
         deleteRow(index) {
           this.rows.splice(index, 1);
+          this.updateRows();
+        },
+
+        updateRows() {
+          this.rows.forEach(row => {
+            row.isLast = false
+          })
+
+          if(this.rows.length > 0) {
+            this.rows[this.rows.length - 1].isLast = true
+            delete this.rows[this.rows.length - 1].dailyLimit
+            delete this.rows[this.rows.length - 1].weeklyLimit
+          }
+
+          this.json = JSON.stringify({
+            uuid: "<UUID>",
+            rules: this.rows
+          }, null, 2)
+        }
+      },
+      computed: {
+        prettyJson() {
+          return JSON.stringify(this.json, null,  2)
         }
       }
     };
@@ -53,25 +81,21 @@
       <div class="row mb-1">&nbsp;</div>
 
       <div class="row" v-if="rows.length > 0">
-        <div class="col-sm">Pay Type</div>
-        <div class="col-sm">Daily Limit</div>
-        <div class="col-sm">Weekly Limit</div>
+        <div class="col-sm mb-2"><b>Pay Type</b></div>
+        <div class="col-sm"><b>Daily Limit</b></div>
+        <div class="col-sm"><b>Weekly Limit</b></div>
         <div class="col-sm"></div>
       </div>
 
 
       <div v-for="(row, index) of rows" class="row">
-        <div class="col-sm">{{ row.payType }}</div>
+        <div class="col-sm mb-2">{{ row.payType }}</div>
         <div class="col-sm">{{ row.dailyLimit == null ? "-" : row.dailyLimit }}</div>
         <div class="col-sm">{{ row.weeklyLimit == null ? "-" : row.weeklyLimit }}</div>
         <div class="col-sm"><button class="btn-danger" @click="deleteRow(index)"><span class="bi-trash"></span></button></div>
       </div>
 
       <div class="row mb-3">
-
-      </div>
-
-      <div class="row">
         <div class="col">
           <form class="form-inline">
             <div v-if="rows.length==0">
@@ -87,13 +111,19 @@
 
             <select id="payType" class="form-control mr-2" v-model="payType">
               <option value="" disabled>Please Select</option>
+              <option>Half Time</option>
               <option>Regular Time</option>
               <option>Double Time</option>
               <option>Triple Time</option>
+              <option>Extra Time</option>
             </select>
             <button type="submit" id="addButton" class="btn-secondary" @click="addRow"><span class="bi-plus-circle"></span></button>
           </form>
         </div>
+      </div>
+
+      <div class="row mb-3" v-if="json != null">
+        <pre>{{ json }}</pre>
       </div>
   </layout-default>
 </template>
